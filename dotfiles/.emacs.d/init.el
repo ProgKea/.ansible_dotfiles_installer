2014062c4c364f94c16e48e1d51fdce25afe8a2e
ProgKea <hoschy01@googlemail.com>
2022-07-30 09:48:11 +0200

    removed which-key

--- dotfiles/.emacs.d/init.el
+++ dotfiles/.emacs.d/init.el
@@ -61,14 +61,12 @@
 (setq compilation-ask-about-save nil)
 (setq scroll-margin 8)
 (setq auto-save-default nil)
-(setq-default word-wrap t)
+;; (setq-default word-wrap t)
 (setq-default indent-tabs-mode nil)
 (setq-default compilation-scroll-output t)
 (setq-default dired-dwim-target t)
 
-;; add-to-lists
 (add-to-list 'default-frame-alist `(font . ,"Iosevka-20"))
-;; (add-to-list 'default-frame-alist `(font . ,"Ubuntu Mono-20"))
 
 (define-minor-mode minor-mode-blackout-mode
   "Hides minor modes from the mode line."
@@ -96,26 +94,6 @@
 (menu-bar--display-line-numbers-mode-relative)
 (fringe-mode 0)
 (blink-cursor-mode 1)
-
-(load "~/.emacs.d/elpa/simpc-mode.el" t)
-
-(add-to-list 'auto-mode-alist '("\\.[hc]\\(pp\\)?\\'" . simpc-mode))
-
-(defun astyle-buffer (&optional justify)
-  (interactive)
-  (let ((saved-line-number (line-number-at-pos)))
-    (shell-command-on-region
-     (point-min)
-     (point-max)
-     "astyle --style=kr"
-     nil
-     t)
-    (goto-line saved-line-number)))
-
-(add-hook 'simpc-mode-hook
-          (lambda ()
-            (interactive)
-            (setq-local fill-paragraph-function 'astyle-buffer)))
 
 ;; Install and configure packages
 (use-package evil
@@ -147,6 +125,7 @@
   (evil-define-key 'visual 'global (kbd "<leader>r") 'query-replace)
   (evil-define-key 'visual 'global (kbd "<leader>R") 'query-replace-regexp)
 
+  (evil-define-key 'normal 'global (kbd "<leader>la") 'fill-paragraph)
   (evil-define-key 'visual 'global (kbd "<leader>a") 'align-regexp)
   (evil-define-key 'visual 'global (kbd "C") 'comment-or-uncomment-region)
 
@@ -168,9 +147,6 @@
   ;; escape minibuffer
   (global-set-key (kbd "C-x ESC") nil)
   (evil-define-key 'normal 'global (kbd "<escape>") 'abort-minibuffers)
-
-  ;; make leader key work in dired mode
-  (with-eval-after-load 'dired (evil-define-key 'normal dired-mode-map (kbd "<SPC>") 'evil-send-leader))
 
   (evil-define-key 'normal 'global (kbd "<leader>ln") 'next-error)
   (evil-define-key 'normal 'global (kbd "<leader>lp") 'previous-error)
@@ -267,10 +243,6 @@
         completion-category-defaults nil
         completion-category-overrides '((file (styles partial-completion)))))
 
-(use-package which-key
-  :init
-  (which-key-mode 1))
-
 (use-package undo-tree
   :after evil
   :diminish
@@ -280,7 +252,26 @@
   :custom
   (undo-tree-history-directory-alist '(("." . "~/.emacs.d/undo"))))
 
-;; Themes
+(use-package astyle
+  :config
+  (load "~/.emacs.d/elpa/simpc-mode.el" t)
+  (add-to-list 'auto-mode-alist '("\\.[hc]\\(pp\\)?\\'" . simpc-mode))
+  (defun astyle-buffer (&optional justify)
+    (interactive)
+    (let ((saved-line-number (line-number-at-pos)))
+      (shell-command-on-region
+       (point-min)
+       (point-max)
+       "astyle --style=kr"
+       nil
+       t)
+      (goto-line saved-line-number)))
+
+  (add-hook 'simpc-mode-hook
+            (lambda ()
+              (interactive)
+              (setq-local fill-paragraph-function 'astyle-buffer))))
+
 (use-package gruber-darker-theme
   :config
   (load-theme 'gruber-darker t))
@@ -300,7 +291,6 @@
   (evil-define-key 'normal 'global (kbd "<leader>lf") 'lsp-find-references)
   (evil-define-key 'normal 'global (kbd "<leader>ld") 'evil-goto-definition)
   (evil-define-key 'normal 'global (kbd "<leader>lc") 'lsp-execute-code-action)
-  (evil-define-key 'normal 'global (kbd "<leader>la") 'lsp-format-buffer)
   (evil-define-key 'normal 'global (kbd "<leader>ll") 'flycheck-list-errors)
   (evil-define-key 'normal 'global (kbd "<leader>lh") 'lsp-ui-doc-glance)
   (evil-define-key 'normal 'global (kbd "<leader>lo") '(lambda () (interactive)
@@ -322,11 +312,12 @@
   (tab-always-indent 'complete)
   (corfu-auto-delay 0.2)
   (corfu-auto-prefix 1)
+  (corfu-preselect-first nil)
   :init
   (global-corfu-mode)
   :config
   (evil-define-key 'insert corfu-map (kbd "C-e") 'corfu-quit)
-  (evil-define-key 'insert corfu-map (kbd "<RET>") 'corfu-complete)
+  (evil-define-key 'insert corfu-map (kbd "<RET>") 'evil-ret)
   (evil-define-key 'insert 'global (kbd "C-<SPC>") 'complete-symbol)
   (evil-define-key 'insert corfu-map (kbd "TAB") 'yas-expand)
   (evil-define-key 'insert corfu-map [tab] 'yas-expand))
@@ -362,19 +353,15 @@
   :init
   (setq zig-format-on-save nil))
 
-;; hooks
+;; dired
 (add-hook 'dired-mode-hook (lambda () (display-line-numbers-mode -1)))
 (add-hook 'dired-mode-hook 'auto-revert-mode)
+(with-eval-after-load 'dired (evil-define-key 'normal dired-mode-map (kbd "<SPC>") 'evil-send-leader))
+
+;; eww
 (add-hook 'eww-mode-hook (lambda () (display-line-numbers-mode -1)))
 
 ;; functions
-(defun kill-other-buffers ()
-  "Kill all other buffers."
-  (interactive)
-  (mapc 'kill-buffer
-        (delq (current-buffer)
-              (cl-remove-if-not 'buffer-file-name (buffer-list)))))
-
 (defun my/jump-to-par (&rest args)
   (when (< (save-excursion
              (search-forward "("))
@@ -391,7 +378,7 @@
  '(custom-safe-themes
    '("a3e99dbdaa138996bb0c9c806bc3c3c6b4fd61d6973b946d750b555af8b7555b" "5586a5db9dadef93b6b6e72720205a4fa92fd60e4ccfd3a5fa389782eab2371b" "e3daa8f18440301f3e54f2093fe15f4fe951986a8628e98dcd781efbec7a46f2" "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" "7a424478cb77a96af2c0f50cfb4e2a88647b3ccca225f8c650ed45b7f50d9525" "7e377879cbd60c66b88e51fad480b3ab18d60847f31c435f15f5df18bdb18184" "e1f4f0158cd5a01a9d96f1f7cdcca8d6724d7d33267623cc433fe1c196848554" "60ada0ff6b91687f1a04cc17ad04119e59a7542644c7c59fc135909499400ab8" default))
  '(package-selected-packages
-   '(move-text astyle zig-mode zenburn-theme yasnippet yaml-mode which-key vertico unicode-escape undo-tree typescript-mode rust-mode quelpa-use-package projectile orderless lsp-pyright iedit hydra haskell-mode gruber-darker-theme frame-local flymake-easy flycheck-nimsuggest epc disable-mouse commenter autothemer auctex async ansible)))
+   '(move-text astyle zig-mode zenburn-theme yasnippet yaml-mode vertico unicode-escape undo-tree typescript-mode rust-mode quelpa-use-package projectile orderless lsp-pyright iedit hydra haskell-mode gruber-darker-theme frame-local flymake-easy flycheck-nimsuggest epc disable-mouse commenter autothemer auctex async ansible)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
